7468e5f92f7e523536e85acc372ebda220c5d6c7
Victor Synko <oomashu@gmail.com>
2021-03-22 23:18:11 +0200

    Commit 1

--- Pr3/Pr3/Main.cpp
+++ Pr3/Pr3/Main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-
+	//Дискриминант x2+2x+6=0
+	std::string input{}, time{};
+	std::int32_t a{}, b{}, c{};
+	std::cout << "Enter your equoution according example(7x2 + 2x - 8 = 0): ";
+	std::getline(std::cin, input);
+	for (auto c : input)
+	{
+		if(c>='0'&&c<=9)
+		{
+			time{c}.......
+		}
+	}
 }
